fb62e9591d2e9d8198ae15a0a5963e3223077c86
Samuel Gasper <1516828@my.brunel.ac.uk>
2018-02-19 12:36:49 +0000

    Done without making it pretty. I do not enjoy CSS

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  # Make a fake database hash containing the questions and the answers
+
+
+
+  def ask
+  end
+
+  def answer
+    @ask = params[:question]
+    if @ask == "I am going to work"
+      @answer = "Great!"
+    elsif @ask.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
